fc9e3659f074feb51cf8cba465308bff691b3398
AnhMV7 <vumin@oregonstate.edu>
2021-09-27 16:04:07 -0700

    flix

---
+++ Pods/Target Support Files/Pods-AnhFlixster-AnhFlixsterUITests/Pods-AnhFlixster-AnhFlixsterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnhFlixster_AnhFlixsterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AnhFlixster_AnhFlixsterUITests
+@end
---
+++ Pods/Target Support Files/Pods-AnhFlixster/Pods-AnhFlixster-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnhFlixster : NSObject
+@end
+@implementation PodsDummy_Pods_AnhFlixster
+@end
