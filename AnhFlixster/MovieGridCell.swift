a9bc18a881663d25f134ea37162380a4392fb762
AnhMV7 <vumin@oregonstate.edu>
2021-10-04 17:40:26 -0700

    Part2

---
+++ AnhFlixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  AnhFlixster
+//
+//  Created by Anh Vu on 9/29/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
